ac3c27341f8e65555c951b4db76cedbf560a56fb
Gubibbles <107900569+Gubibbles@users.noreply.github.com>
2026-02-10 10:15:26 -0300

    aula 1

---
+++ Aula01/AulaInaugural/src/Variáveis.java
@@ -0,0 +1,15 @@
+public class Variáveis {
+    public static void main(String[] args) {
+
+        int idade = 35;
+
+        double altura = 1.80;
+        double peso = 85.90;
+
+        char genero = 'F'; //Caracteres - ''
+        String nome = "Fulano"; //String - ""
+
+        System.out.println("Nome:" + nome + "Idade:" + idade + "Altura:" + altura + "Peso:" + peso + "Genero:" + genero);
+
+    }
+}
