ddf66a3fda9373169de44bb2995b17dd1318752e
Gubibbles <mrsgbbs@hotmail.com>
2026-05-07 10:31:03 -0300

    .

--- SpringBoot/demo/src/main/java/com/example/demo/controller/ControllerProduto.java
+++ SpringBoot/demo/src/main/java/com/example/demo/controller/ControllerProduto.java
@@ -2,17 +2,27 @@ package com.example.demo.controller;
 
 
 import com.example.demo.model.DadosCadastroRoupa;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.example.demo.model.Roupa;
+import com.example.demo.repository.RoupaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/roupa")
 public class ControllerProduto {
+
+    @Autowired
+    private RoupaRepository roupaRepository;
+
     @PostMapping
     public void cadastrarProduto(@RequestBody DadosCadastroRoupa dados) {
-        System.out.println(dados);
+        roupaRepository.save(new Roupa(dados));
+    }
 
+    @GetMapping
+    public List<Roupa> listarProduto() {
+        return roupaRepository.findAll();
     }
 }
---
+++ SpringBoot/demo/src/main/java/com/example/demo/repository/RoupaRepository.java
@@ -0,0 +1,7 @@
+package com.example.demo.repository;
+
+import com.example.demo.model.Roupa;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RoupaRepository extends JpaRepository<Roupa, Integer> {
+}
